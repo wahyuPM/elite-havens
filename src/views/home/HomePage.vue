44e5affede8fa9af4a2bbd2d2b7d3b753bc67e30
wahyuPM <wahyupratama191@gmail.com>
2022-07-04 23:39:25 +0800

    add : init elite section

--- src/views/home/HomePage.vue
+++ src/views/home/HomePage.vue
@@ -1,6 +1,29 @@
 <template>
   <div>
     <Hero />
+    <section class="elite">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-6">
+            <div class="elite-content w-75">
+              <h3>
+                HAVENS AREN’T PLACES TO SHELTER FROM THE WORLD. THEY’RE PLACES
+                TO EMBRACE IT.
+              </h3>
+              <p class="mb-4">
+                Across a meticulously-curated global portfolio of close to 300
+                private sanctuaries, we transcend beauty to offer tailored
+                personal service and unparalleled experiences that set the
+                standard
+              </p>
+              <a href="#" class="btn btn-primary rounded-pill">
+                See what makes us different
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     <div class="demo-page"></div>
     <Modal />
   </div>
