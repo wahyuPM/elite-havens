e87e579e77db00ac1611112f6a4b5d7e5ea1a202
wahyuPM <wahyupratama191@gmail.com>
2022-07-05 23:13:58 +0800

    add : elite section

--- src/views/home/HomePage.vue
+++ src/views/home/HomePage.vue
@@ -4,8 +4,8 @@
     <section class="elite">
       <div class="container">
         <div class="row align-items-center">
-          <div class="col-md-6">
-            <div class="elite-content w-75">
+          <div class="col-md-6 d-flex justify-content-center">
+            <div class="elite-content">
               <h3>
                 HAVENS AREN’T PLACES TO SHELTER FROM THE WORLD. THEY’RE PLACES
                 TO EMBRACE IT.
@@ -16,10 +16,24 @@
                 personal service and unparalleled experiences that set the
                 standard
               </p>
-              <a href="#" class="btn btn-primary rounded-pill">
+              <a href="#" class="btn btn-primary elite__btn rounded-pill">
                 See what makes us different
               </a>
             </div>
+          </div>
+          <div class="col-md-6 border-left">
+            <div class="elite-img ps-4">
+              <img src="@/assets/img/elite.jpg" alt="elite-hero" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="tagline">
+      <div class="container tagline-content">
+        <div class="row">
+          <div class="col-12">
+            <h2>Your journey to total freedom starts here</h2>
           </div>
         </div>
       </div>
